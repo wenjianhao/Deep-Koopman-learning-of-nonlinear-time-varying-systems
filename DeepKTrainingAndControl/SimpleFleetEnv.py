1641cde76686e66844828a748021c788f8eef8da
Wenjian Hao <haowjz@gmail.com>
2021-09-29 22:18:43 -0400

    Update SimpleFleetEnv.py

--- DeepKTrainingAndControl/SimpleFleetEnv.py
+++ DeepKTrainingAndControl/SimpleFleetEnv.py
@@ -22,46 +22,45 @@ class SimpleFleet(object):
         '''
         self.instates = initial_states 
         self.simtime = simtime
-        self.state1 = []
-        self.state2 = []       
-        self.state3 = [] 
-        self.state4 = [] 
-        self.control1 = [] 
-        self.control2 = [] 
-        self.control3 = [] 
-        self.control4 = [] 
-        self.state1.append(self.instates[0])
-        self.state1.append(self.instates[1])
-        self.state2.append(self.instates[2])
-        self.state3.append(self.instates[3])
-        self.state3.append(self.instates[4])
-        self.state4.append(self.instates[5])
-        self.control1.append(self.instates[6])
-        self.control3.append(self.instates[7])
+        self.state1 = np.zeros(simtime)
+        self.state2 = np.zeros(simtime)     
+        self.state3 = np.zeros(simtime) 
+        self.state4 = np.zeros(simtime) 
+        self.control1 = np.zeros(simtime) 
+        self.control2 = np.zeros(simtime)  
+        self.control3 = np.zeros(simtime) 
+        self.control4 = np.zeros(simtime) 
+        self.state1[0] = self.instates[0]
+        self.state1[1] = self.instates[1]
+        self.state2[0] = self.instates[2]
+        self.state3[0] = self.instates[3]
+        self.state3[1] = self.instates[4]
+        self.state4[0] = self.instates[5]
+        self.control1[0] = self.instates[6]
+        self.control3[0] = self.instates[7]
 
     def sim(self, u, currentsteps):
+        u = np.squeeze(u)
         if currentsteps >= 2:
             u1t = u[0]
             u3t = u[2]
-            self.state1[currentsteps] = self.state1[currentsteps-2] * self.state1[currentsteps-1] / (1 + self.state1[currentsteps-2]**2 + self.state1[currentsteps-1]**2) + 3*u1t
-            self.state3[currentsteps] = (self.state3[currentsteps-2] * self.state3[currentsteps-1] * u3t + u3t)/(1+self.state3[currentsteps-2]**2 + self.state3[currentsteps-1]**2)
-            self.control1[currentsteps-1] = u1t
-            self.control3[currentsteps-1] = u3t
+            self.state1[currentsteps] = self.state1[currentsteps-2] * self.state1[currentsteps-1] / (1 + (self.state1[currentsteps-2])**2 + (self.state1[currentsteps-1])**2) + 3*u1t
+            self.state3[currentsteps] = (self.state3[currentsteps-2] * self.state3[currentsteps-1] * u3t + u3t)/(1+(self.state3[currentsteps-2])**2 + (self.state3[currentsteps-1])**2)
         if currentsteps >= 1:
             u2t = u[1]
             u4t = u[3]
-            self.state2[currentsteps] = self.state2[currentsteps-1]/(1+self.state2[currentsteps-1]**4) + u2t**4
-            self.state4[currentsteps] = self.state4[currentsteps-1] * u4t / (1+self.state4[currentsteps-1]**4) +2*u4t
-            self.control2[currentsteps-1] = u2t
-            self.control4[currentsteps-1] = u4t
-        if self.state1[currentsteps] < 0 or self.state2[currentsteps] < 0 or self.state3[currentsteps] < 0 or self.state4[currentsteps] < 0 \
+            self.state2[currentsteps] = self.state2[currentsteps-1]/(1+(self.state2[currentsteps-1])**4) + (u2t)**4
+            self.state4[currentsteps] = self.state4[currentsteps-1] * u4t / (1+(self.state4[currentsteps-1])**4) +2*u4t
+        if currentsteps > 0 and (self.state1[currentsteps] < 0 or self.state2[currentsteps] < 0 or self.state3[currentsteps] < 0 or self.state4[currentsteps] < 0 \
             or currentsteps > self.simtime \
-                or self.state1[currentsteps] == self.state2[currentsteps] or self.state1[currentsteps] == self.state3[currentsteps] \
-                    or self.state1[currentsteps] == self.state4[currentsteps] or self.state2[currentsteps] == self.state3[currentsteps] \
-                        or self.state2[currentsteps] == self.state4[currentsteps] or self.state3[currentsteps] == self.state4[currentsteps]:
+                or self.state1[currentsteps] == self.state2[currentsteps] !=0 or self.state1[currentsteps] == self.state3[currentsteps]!=0 \
+                    or self.state1[currentsteps] == self.state4[currentsteps]!=0 or self.state2[currentsteps] == self.state3[currentsteps]!=0 \
+                        or self.state2[currentsteps] == self.state4[currentsteps]!=0 or self.state3[currentsteps] == self.state4[currentsteps]!=0):
             done = True
+            print('state', [self.state1[currentsteps], self.state2[currentsteps], self.state3[currentsteps], self.state4[currentsteps]])
         else:
             done = False
+
         return self.state1[currentsteps], self.state2[currentsteps], self.state3[currentsteps], self.state4[currentsteps], done
 
     def reset(self):
