3f6dd198643a4df038ec8260a2d6e590a785f512
Wenjian Hao <haowjz@gmail.com>
2024-02-04 11:28:03 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 Description: Paper codes of 'Hao, Wenjian, Bowen Huang, Wei Pan, Di Wu, and Shaoshuai Mou. "Deep Koopman learning of nonlinear time-varying systems." 
                             Automatica 159 (2024): 111372.'
 
-Author: Wenjian Hao, Purdue University
+Author: Wenjian Hao, Purdue Universibar_X_mat
 
 This is a file containing all the functions.
 
@@ -11,8 +11,6 @@ Last Revision: Jan 2022
 '''
 
 import torch
-import config
-import control
 
 import numpy as np
 import torch.optim as optim
@@ -25,80 +23,75 @@ from torchsummary import summary
 # training loop
 #=====================
 class DKTV_training(object):
-    
+    '''
+    Deep Koopman learning for nonlinear time-varying systems
+    '''
     def __init__(self, config: dict):
-      self.plot_NN = config.flags['plot_NN']
-      self.keep_train = config.flags['keep_train']
-
-      self.results_path = config.files_dir['results_path']
-      self.data_name = config.files_dir['data_name']
+      # load the configureation parameters
+      self.plot_NN          = config.flags['plot_NN']
+      self.keep_train       = config.flags['keep_train']
+      self.results_path     = config.files_dir['results_path']
+      self.data_name        = config.files_dir['data_name']
       self.model_saved_name = config.files_dir['model_saved_name'] 
-      
-      self.dim_states = config.dimensions['dim_states']
-      self.dim_lifting = config.dimensions['dim_lifting']
-      
-      self.eps = config.training_parameters['eps']
-      self.decay_rate = config.training_parameters['decay_rate']
-      self.learning_rate = config.training_parameters['learning_rate']
-      self.training_epoch = config.training_parameters['training_epoch']
-
-      # training devices, uncomment for gpu
+      self.dim_states       = config.dimensions['dim_states']
+      self.dim_lifting      = config.dimensions['dim_lifting']
+      self.eps              = config.training_parameters['eps']
+      self.decay_rate       = config.training_parameters['decay_rate']
+      self.learning_rate    = config.training_parameters['learning_rate']
+      self.training_epoch   = config.training_parameters['training_epoch']
+      self.training_device  = torch.device("cpu")
+      # training devices, uncomment it to use gpu
       # print("Torch version: {}".format(torch.__version__))
       # cuda = torch.cuda.is_available()
       # self.training_device = torch.device("cuda" if cuda else "cpu")
       # print("Training device {}".format(self.training_device))
-      self.training_device = torch.device("cpu")
-
-    def pretrain_model(self, tx, ty):
+      
+    def pretrain_model(self, X_mat, bar_X_mat):
       # some flags
       # keep training?
       if self.keep_train:
         load_liftNN = self.results_path + self.model_saved_name
         ckpt_liftNN = torch.load(load_liftNN, map_location=self.training_device)
-        lifting = LNN(dim_input=self.dim_states, dim_output=self.dim_lifting)
+        lifting     = LNN(dim_input=self.dim_states, dim_output=self.dim_lifting)
         lifting.load_state_dict(ckpt_liftNN['model_lifting'])
         print('Keep Training')
       else:
         lifting = LNN(dim_input=self.dim_states, dim_output=self.dim_lifting)
       # plot DNNs structure
       if self.plot_NN:
-          device = self.training_device
+          device       = self.training_device
           modellifting = LNN(dim_input=self.dim_states, dim_output=self.dim_lifting).to(device)
           summary(modellifting, (1,self.dim_states))
-
-      # Training
-      train_loss = []
       # get the training data and pass it to the training GPU
-      trset = torch.t(torch.FloatTensor(tx).to(self.training_device))
-      trlab = torch.t(torch.FloatTensor(ty).to(self.training_device))
+      X_mat     = torch.t(torch.FloatTensor(X_mat).to(self.training_device))
+      bar_X_mat = torch.t(torch.FloatTensor(bar_X_mat).to(self.training_device))
       lifting.to(self.training_device)
       # choose the training optimizer
-      optimizer = optim.Adam(lifting.parameters(), lr=self.learning_rate, weight_decay = self.decay_rate)
+      optimizer  = optim.Adam(lifting.parameters(), lr=self.learning_rate, weight_decay = self.decay_rate)
+      train_loss = []
       lifting.train()
       for i in range(self.training_epoch):
           # lifting xt and x(t+1)
-          x_t_lift = lifting(trset)
-          x_t1_lift = lifting(trlab)
+          G_mat     = lifting(X_mat)
+          bar_G_mat = lifting(bar_X_mat)
           # get the A, C matrices
-          A = torch.t(x_t1_lift)@torch.pinverse(torch.t(x_t_lift))
-          C = trset.T@torch.pinverse(x_t_lift.T)
+          A_mat = torch.t(bar_G_mat)@torch.pinverse(torch.t(G_mat))
+          C_mat = X_mat.T@torch.pinverse(G_mat.T)
           # get the K matrix in paper
-          AC_mat = torch.cat((A, C), 0)
-          AC_mat = np.matrix(AC_mat.cpu().detach().numpy())
-          AC_mat = torch.FloatTensor(AC_mat).to(self.training_device)
+          AC_mat = torch.cat((A_mat, C_mat), 0).to(self.training_device)
           # loss1 is the linear lifting loss
-          loss1, p1 = self.linearize_loss(AC_mat, x_t_lift, x_t1_lift, trset)
-          total_loss = loss1
+          L_f = self.linearize_loss(AC_mat.detach(), G_mat, bar_G_mat, X_mat)
+          total_loss = L_f
           # gradient descent
-          if total_loss > self.eps:
-            optimizer.zero_grad()
-            total_loss.backward()
-            optimizer.step()
+          optimizer.zero_grad()
+          total_loss.backward()
+          optimizer.step()
           # only saving the model with the lowest loss
           train_loss.append(total_loss.cpu().detach().numpy())
           min_loss = min(train_loss)
-          if total_loss.cpu().detach().numpy() <= min_loss:   
-              state = {'model_lifting': lifting.state_dict()}
+          if total_loss.cpu().detach().numpy() <= min_loss:
+              data_his_c  = torch.inverse(G_mat.T@G_mat)    
+              state       = {'model_lifting': lifting.state_dict()}
               torch.save(state, (self.results_path+'nnbasis/'+str(0)+self.model_saved_name))
               print("Saved min loss model, loss: ", total_loss.cpu().detach().numpy())
           # break if the esitimation accuracy is satisfied
@@ -106,84 +99,71 @@ class DKTV_training(object):
               state = {'model_lifting': lifting.state_dict()}
               torch.save(state, (self.results_path+'nnbasis/'+str(0)+self.model_saved_name))
               break
-      return A.cpu().detach().numpy(), C.cpu().detach().numpy()
+      return A_mat.cpu().detach().numpy(), C_mat.cpu().detach().numpy(), data_his_c.cpu().detach().numpy()
 
-    def DKTV(self, Astk, tx, ty, batchsize, nd):
+    def DKTV(self, Astk, Cstk, Hisstk, X_mat, bar_X_mat, batchsize, nd):
       # load pretrained DNN
       load_liftNN = self.results_path +'nnbasis/'+str(nd-1) + self.model_saved_name
       ckpt_liftNN = torch.load(load_liftNN, map_location=self.training_device)
-      lifting = LNN(dim_input=self.dim_states, dim_output=self.dim_lifting)
+      lifting     = LNN(dim_input=self.dim_states, dim_output=self.dim_lifting)
       lifting.load_state_dict(ckpt_liftNN['model_lifting'])
       print('Finish loading the pretrained model')
-      # plot DNN structure or not
-      if self.plot_NN:
-          device = torch.device("cuda")
-          modellifting = LNN(dim_input=self.dim_states, dim_output=self.dim_lifting).to(device)
-          summary(modellifting, (1,self.dim_states))
-      # Training
       # pass data to the training device
-      trset = torch.t(torch.FloatTensor(tx).to(self.training_device))
-      trlab = torch.t(torch.FloatTensor(ty).to(self.training_device))
+      X_mat     = torch.t(torch.FloatTensor(X_mat).to(self.training_device))
+      bar_X_mat = torch.t(torch.FloatTensor(bar_X_mat).to(self.training_device))
       lifting.to(self.training_device)
-      fixlifting = lifting
       # split the data by time
-      xold = trset[0:(trset.size(dim=0)-batchsize), :]
-      yold = trlab[0:(trlab.size(dim=0)-batchsize), :]
-      xnew = trset[(trset.size(dim=0)-batchsize-1):, :]
-      ynew = trlab[(trset.size(dim=0)-batchsize-1):, :]
+      xnew = X_mat[(X_mat.size(dim=0)-batchsize-1):, :]
+      ynew = bar_X_mat[(X_mat.size(dim=0)-batchsize-1):, :]
       # previous solution
-      x_t_old = fixlifting(xold)
-      x_t1_old = fixlifting(yold)
-      XU = torch.t(x_t_old)
-      G = XU@torch.t(XU)
-      Ap = torch.FloatTensor(Astk[:,:,(nd-1)]).to(self.training_device)
+      A_mat_tau   = torch.FloatTensor(Astk[:,:,(nd-1)]).to(self.training_device)
+      C_mat_tau   = torch.FloatTensor(Cstk[:,:,(nd-1)]).to(self.training_device)
+      His_mat_tau = torch.FloatTensor(Hisstk[:,:,(nd-1)]).to(self.training_device)
       # choose the training optimizer
       optimizer = optim.Adam(lifting.parameters(), lr=self.learning_rate, weight_decay = self.decay_rate)
       train_loss = []
       lifting.train()
+      # lifting xt and x(t+1)
+      G_mat      = lifting(xnew)
+      bar_G_mat  = lifting(ynew)          
+      lambdatau  = torch.inverse(torch.eye(G_mat.T.size(dim=1)).to(self.training_device)+G_mat@His_mat_tau@G_mat.T)
+      A_mat_tau1 = A_mat_tau + (torch.t(bar_G_mat) - A_mat_tau@G_mat.T)@lambdatau@torch.t(G_mat.T)@His_mat_tau
+      C_mat_tau1 = C_mat_tau + (torch.t(xnew) - C_mat_tau@G_mat.T)@lambdatau@G_mat@His_mat_tau
+      # get the K matrix in paper
+      AC_mat = torch.cat((A_mat_tau1, C_mat_tau1), 0).to(self.training_device)
       for i in range(self.training_epoch):
           # lifting xt and x(t+1)
-          x_t_lift = lifting(xnew)
-          x_t1_lift = lifting(ynew)          
-          try:
-            chinew = torch.t(x_t_lift)
-            lambdatau = torch.inverse(torch.eye(chinew.size(dim=1)).to(self.training_device)+torch.t(chinew)@torch.inverse(G)@chinew)
-            A = Ap + (torch.t(x_t1_lift) - Ap@chinew)@lambdatau@torch.t(chinew)@torch.inverse(G)  
-            C = torch.t(xnew)@torch.pinverse(torch.t(x_t_lift))
-          except:
-            print('Please check the data, it may not be full rank or have Nah value')
-          # get the K matrix in paper
-          AC_mat = torch.cat((A, C), 0)
-          AC_mat = np.matrix(AC_mat.cpu().detach().numpy())
-          AC_mat = torch.FloatTensor(AC_mat).to(self.training_device)
-          loss1, p1 = self.linearize_loss(AC_mat, x_t_lift, x_t1_lift, xnew)
-          # gradient descent 
-          total_loss = loss1
-          if total_loss > self.eps:
-            optimizer.zero_grad()
-            total_loss.backward()
-            optimizer.step()
+          G_mat     = lifting(xnew)
+          bar_G_mat = lifting(ynew)          
+          L_f       = self.linearize_loss(AC_mat.detach(), G_mat, bar_G_mat, xnew)
+          # gradient descent, add other loss functions if you have
+          total_loss = L_f
+          optimizer.zero_grad()
+          total_loss.backward()
+          optimizer.step()
           # only saving the model with the lowest loss
           train_loss.append(total_loss.cpu().detach().numpy())
           min_loss = min(train_loss)
-          if total_loss.cpu().detach().numpy() <= min_loss:   
+          if total_loss.cpu().detach().numpy() <= min_loss: 
+            data_his_c  = torch.inverse(G_mat.T@G_mat)  
             state = {'model_lifting': lifting.state_dict()}
             torch.save(state, (self.results_path+'nnbasis/'+str(nd)+self.model_saved_name))
             print("Saved min loss model, loss: ", total_loss.cpu().detach().numpy())
           if total_loss <= self.eps:
+            data_his_c  = torch.inverse(G_mat.T@G_mat)
             state = {'model_lifting': lifting.state_dict()}
             torch.save(state, (self.results_path+'nnbasis/'+str(nd)+self.model_saved_name))
             break
-      return A.cpu().detach().numpy(), C.cpu().detach().numpy()
+      return A_mat_tau1.cpu().detach().numpy(), C_mat_tau1.cpu().detach().numpy(), data_his_c.cpu().detach().numpy()
     
     #=====================
     # loss functions
     #=====================
-    def linearize_loss(self, AC_mat, x_t_lift, x_t1_lift, xnew):
-      label = torch.cat((x_t1_lift.T, xnew.T), 0)
-      p1 = AC_mat[0:self.dim_lifting, :] @ x_t_lift.T
-      p2 = AC_mat[self.dim_lifting:, :] @ x_t_lift.T
+    def linearize_loss(self, AC_mat, G_mat, bar_G_mat, xnew):
+      label = torch.cat((bar_G_mat.T, xnew.T), 0)
+      p1 = AC_mat[0:self.dim_lifting, :] @ G_mat.T
+      p2 = AC_mat[self.dim_lifting:, :] @ G_mat.T
       prediction = torch.cat((p1, p2), 0)
       lossfunc = torch.nn.MSELoss()
-      loss1 = lossfunc(prediction, label)
-      return loss1, p1
+      L_f = lossfunc(prediction, label)
+      return L_f
